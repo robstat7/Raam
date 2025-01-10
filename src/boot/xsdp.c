fbb3bbba149df45febcbb6228a40bb6240ca4e86
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2025-01-10 15:00:18 +0530

    refactoring

--- src/boot/xsdp.c
+++ src/boot/xsdp.c
@@ -1,22 +1,34 @@
 #include <boot/xsdp.h>
 
 // EFI GUID for a pointer to the ACPI 2.0 or later specification XSDP structure
-EFI_GUID Acpi20TableGuid = ACPI_20_TABLE_GUID;
+EFI_GUID acpi_20_table_guid = ACPI_20_TABLE_GUID;
 
 void *xsdp = NULL;
 
+/*
+ * get_xsdp_pointer
+ *
+ * This function gets the xsdp structure pointer by examining the EFI
+ * Configuration Table within the EFI System Table.
+ * The boot loader must retrieve the pointer to the xsdp
+ * structure before assuming platform control via the EFI
+ * ExitBootServices interface.
+ */
 int get_xsdp_pointer(EFI_SYSTEM_TABLE *system_table)
 {
-	int num_config_tables = system_table->NumberOfTableEntries;
+	UINTN num_config_tables = system_table->NumberOfTableEntries;
 	int ret = 0;
-
+	struct xsdp_struct *table;
 	EFI_CONFIGURATION_TABLE *config_tables = system_table->ConfigurationTable;
 
-	for(int i = 0; i < num_config_tables; i++) {
-                if(CompareGuid(&config_tables[i].VendorGuid, &Acpi20TableGuid) == 0) {
-                        uint8_t *table = config_tables[i].VendorTable;
-			// check if ACPI version is >= 2.0
-			uint8_t revision = *(table + 15);	
+	// check if any table has the valid efi guid for the xsdp structure
+	for(UINTN i = 0; i < num_config_tables; i++) {
+                if(CompareGuid(&config_tables[i].VendorGuid, &acpi_20_table_guid) == 0) {
+                        table = (struct xsdp_struct *) config_tables[i].VendorTable;
+			// found a xsdp table. It might be a valid xsdp. Perform
+			// validations.
+			// validation 1: check if ACPI version is >= 2.0
+			uint8_t revision = table->revision;	
 			if(revision == 0x2) {
 				xsdp = (void *) table;
 				break;
--- src/include/boot/xsdp.h
+++ src/include/boot/xsdp.h
@@ -3,6 +3,20 @@
 
 #include <efi.h>
 #include <efilib.h>
+#include <stdint.h>
+
+struct xsdp_struct {
+	char signature[8];
+	uint8_t checksum;
+	char oem_id[6];
+	uint8_t revision;
+	uint32_t rsdt_address;      // deprecated since version 2.0
+	
+	uint32_t length;
+	uint64_t xsdt_address;
+	uint8_t extendedchecksum;
+	uint8_t reserved[3];
+};
 
 int get_xsdp_pointer(EFI_SYSTEM_TABLE *system_table);
 
