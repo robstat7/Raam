eace6dad4c3bf0b14a32363fd00333ee2f0a69bb
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2025-01-12 08:28:19 +0530

    add comments

--- src/kernel/printk.c
+++ src/kernel/printk.c
@@ -1,11 +1,27 @@
 #include <raam/printk.h>
 
+/*
+ * printk
+ *
+ * This function prints the passed format string and the required
+ * arguments to the terminal. It can be called as follows:
+ *
+ *		printk("Raam Raam sa");
+ *
+ * And with a format specifier as follows:
+ *
+ *		printk("error: couldn't find the controller."
+		       "Error code is {d}.", code);
+ *
+ * This is a variadic function. It also changes the `state` from normal
+ * to format specifier and back to print the variable arguments.
+ */
 void printk(const char *format, ...)
 {
 	va_list args;
-	va_start(args, format);
+	va_start(args, format);	// first, initialize the va_list
 	int state = NORMAL;	// begin with normal state
-	char *specifier;
+	char *specifier;	// will contain the specifier letter
 
 	for(int i = 0; format[i] != '\0'; i++) {
 		char current = format[i];
@@ -29,7 +45,7 @@ void printk(const char *format, ...)
 		}
 	}
 
-	va_end(args);
+	va_end(args);	// clean up the va_list
 }
 
 static void print_arg(const char *specifier, va_list *args)
