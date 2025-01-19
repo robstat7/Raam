2a00a8c51b8d2136cf0500dfa73418bf3096e8f0
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2025-01-19 14:58:02 +0530

    mkfs_ext2: rewrite it for simplicity

---
+++ src/include/raam/fs.h
@@ -0,0 +1,30 @@
+#ifndef FS_H
+#define FS_H
+
+#include <stdint.h>
+
+#define BLOCKS_PER_GROUP		256	/* for simplicity */
+
+#define EXT4_LOG_BLOCK_SIZE		0
+
+struct superblock_struct {
+	uint32_t s_inodes_count;
+	uint32_t s_blocks_count;
+	uint32_t s_r_blocks_count;
+	uint32_t s_free_blocks_count;
+	uint32_t s_free_inodes_count;
+	uint32_t s_first_data_block;
+	uint32_t s_log_block_size;
+	uint32_t s_blocks_per_group;
+	uint32_t s_inodes_per_group;
+	char padding1[12];	/* padding to get the magic signature */
+				/* at the same offset as in the */
+				/* revision 1. */
+	uint16_t s_magic;
+	char padding2[18];	/* padding to get the revision level */
+				/* at the same offset as in the */
+				/* revision 1. */
+	uint32_t s_rev_level;
+}__attribute__((packed));
+
+#endif	/* FS_H */
--- src/tools/mkfs_ext2.c
+++ src/tools/mkfs_ext2.c
@@ -16,8 +16,7 @@
  * - we will not use fragments in this implementation.
  */
 #include <stdio.h>
-#include <fs/fs.h>
-#include <time.h>
+#include <raam/fs.h>
 
 #define EXT2_MIN_BLOCK_SIZE		1024
 
@@ -28,7 +27,6 @@
 
 
 const int block_size = 1024;	// we will only support this for now
-const int frag_size = 1024;	// fragment size
 
 /*
  *   - `s_blocks_count`: This value must be lower or equal to
@@ -51,12 +49,13 @@ int main(void)
 	// printf("opened volume.\n");
 
 	// offset to superblock
-	// lseek(fd, 1024, SEEK_SET);
+	// lseek(fd, 1024, SEEK_SET);	
+
+
+	const int total_blocks = 256;	// It is the total blocks in the partition 
+
 
 	struct superblock_struct sb;
-	
-
-	const int total_blocks = 24828;	// It is the total blocks in the partition 
 
 	sb.s_blocks_per_group = BLOCKS_PER_GROUP;
 
@@ -66,69 +65,69 @@ int main(void)
 	int last_group_blocks = total_blocks % BLOCKS_PER_GROUP;
 	sb.s_blocks_count = (full_groups * BLOCKS_PER_GROUP) + last_group_blocks;
 
-	/* calculate s_inodes_count. TODO: verify it using spec */
+	// /* calculate s_inodes_count. TODO: verify it using spec */
 
-	const int inode_ratio = 16384;	/* see the starting of file for notes */
+	// const int inode_ratio = 16384;	/* see the starting of file for notes */
 
-	uint32_t total_inodes = (total_blocks * EXT2_MIN_BLOCK_SIZE) / inode_ratio;
-
-
-	/* calculate reserved blocks count */
-
-	const unsigned reserved_ratio = 5;	// 5% reserved blocks for super user
-	uint32_t total_reserved_blocks = (total_blocks * reserved_ratio) / 100;
-
-	sb.s_r_blocks_count = total_reserved_blocks;
+	// uint32_t total_inodes = (total_blocks * EXT2_MIN_BLOCK_SIZE) / inode_ratio;
 
 
-	/* calculate total inodes per group */
+	// /* calculate reserved blocks count */
 
-	const int frags_per_block = block_size / frag_size; /* fragments per block */
+	// const unsigned reserved_ratio = 5;	// 5% reserved blocks for super user
+	// uint32_t total_reserved_blocks = (total_blocks * reserved_ratio) / 100;
 
-	/* divide the total usable blocks by blocks per group. Block 0 is not usable */
-	const unsigned long group_desc_count = (((total_blocks - NORM_FIRSTBLOCK) * frags_per_block) / BLOCKS_PER_GROUP);	/* number of group descriptors */
+	// sb.s_r_blocks_count = total_reserved_blocks;
 
-	int inodes_per_group = 0;
-	/* round the inodes per group count to fully use each group descriptor */
-        if (sb.s_inodes_count % group_desc_count)                                          
-                inodes_per_group = (sb.s_inodes_count / group_desc_count) + 1;               
-         else                                                                   
-                inodes_per_group = sb.s_inodes_count / group_desc_count;               
 
-	/* Round the inodes pet group count to fully use each block in each descriptor */         
-        if (inodes_per_group % EXT2_INODES_PER_BLOCK)                             
-                inodes_per_group = ((inodes_per_group / EXT2_INODES_PER_BLOCK) + 1) *
-                                   EXT2_INODES_PER_BLOCK;                       
-        total_inodes = inodes_per_group * group_desc_count;
+	// /* calculate total inodes per group */
 
-	
-	sb.s_inodes_count = total_inodes;
-	sb.s_inodes_per_group = inodes_per_group;	
+	// const int frags_per_block = block_size / frag_size; /* fragments per block */
 
-	/* id of the block containing the superblock structure. */
-	sb.s_first_data_block = NORM_FIRSTBLOCK;
+	// /* divide the total usable blocks by blocks per group. Block 0 is not usable */
+	// const unsigned long group_desc_count = (((total_blocks - NORM_FIRSTBLOCK) * frags_per_block) / BLOCKS_PER_GROUP);	/* number of group descriptors */
 
-	sb.s_log_block_size = EXT4_LOG_BLOCK_SIZE;	/* 1024 = 1024 << 0 */
+	// int inodes_per_group = 0;
+	// /* round the inodes per group count to fully use each group descriptor */
+        // if (sb.s_inodes_count % group_desc_count)                                          
+        //         inodes_per_group = (sb.s_inodes_count / group_desc_count) + 1;               
+        //  else                                                                   
+        //         inodes_per_group = sb.s_inodes_count / group_desc_count;               
 
-	sb.s_log_frag_size = EXT4_LOG_FRAG_SIZE;	/* 0 for 1024 */
+	// /* Round the inodes pet group count to fully use each block in each descriptor */         
+        // if (inodes_per_group % EXT2_INODES_PER_BLOCK)                             
+        //         inodes_per_group = ((inodes_per_group / EXT2_INODES_PER_BLOCK) + 1) *
+        //                            EXT2_INODES_PER_BLOCK;                       
+        // total_inodes = inodes_per_group * group_desc_count;
 
-	uint32_t frags_per_group = BLOCKS_PER_GROUP * (block_size / frag_size);
+	// 
+	// sb.s_inodes_count = total_inodes;
+	// sb.s_inodes_per_group = inodes_per_group;	
 
-	sb.s_frags_per_group = frags_per_group;
+	// /* id of the block containing the superblock structure. */
+	// sb.s_first_data_block = NORM_FIRSTBLOCK;
 
-	sb.s_mtime = 0;
+	// sb.s_log_block_size = EXT4_LOG_BLOCK_SIZE;	/* 1024 = 1024 << 0 */
 
-	sb.s_wtime = time(NULL);
+	// sb.s_log_frag_size = EXT4_LOG_FRAG_SIZE;	/* 0 for 1024 */
+
+	// uint32_t frags_per_group = BLOCKS_PER_GROUP * (block_size / frag_size);
+
+	// sb.s_frags_per_group = frags_per_group;
+
+	// sb.s_mtime = 0;
+
+	// sb.s_wtime = time(NULL);
 
 	/* print values */
 	printf("block size = %d bytes\n", block_size);
 	printf("total blocks in the partition = %d\n", total_blocks);
 	printf("sb.s_blocks_per_group = %d\n", sb.s_blocks_per_group);
 	printf("sb.s_blocks_count = %d\n", sb.s_blocks_count);
-	printf("sb.s_inodes_count = %d\n", sb.s_inodes_count);
-	printf("sb.s_r_blocks_count = %d\n", sb.s_r_blocks_count);
-	printf("sb.s_inodes_per_group = %d\n", sb.s_inodes_per_group);
-	printf("sb.s_wtime = %d\n", sb.s_wtime);
+	// printf("sb.s_inodes_count = %d\n", sb.s_inodes_count);
+	// printf("sb.s_r_blocks_count = %d\n", sb.s_r_blocks_count);
+	// printf("sb.s_inodes_per_group = %d\n", sb.s_inodes_per_group);
+	// printf("sb.s_wtime = %d\n", sb.s_wtime);
 
 	return 0;
 }	
