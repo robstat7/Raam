8ccb38b8ff67bbafe291d13729ca923b9d4b48c3
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2025-05-02 14:45:50 +0530

    find file in the root dir

--- src/include/raam/fs/open.h
+++ src/include/raam/fs/open.h
@@ -2,6 +2,7 @@
 #define OPEN_H
 
 #define EXT2_BLOCK_SIZE		4096	/* in bytes */
+#define EXT2_N_BLOCKS		15
 
 /*
  * Structure of a blocks group descriptor
@@ -16,6 +17,77 @@ struct ext2_group_desc
 	uint16_t	bg_used_dirs_count;	/* Directories count */
 	uint16_t	bg_pad;
 	uint32_t	bg_reserved[3];
+};
+
+/*
+ * Structure of an inode on the disk
+ */
+struct ext2_inode {
+	uint16_t	i_mode;		/* File mode */
+	uint16_t	i_uid;		/* Low 16 bits of Owner Uid */
+	uint32_t	i_size;		/* Size in bytes */
+	uint32_t	i_atime;	/* Access time */
+	uint32_t	i_ctime;	/* Creation time */
+	uint32_t	i_mtime;	/* Modification time */
+	uint32_t	i_dtime;	/* Deletion Time */
+	uint16_t	i_gid;		/* Low 16 bits of Group Id */
+	uint16_t	i_links_count;	/* Links count */
+	uint32_t	i_blocks;	/* Blocks count */
+	uint32_t	i_flags;	/* File flags */
+	union {
+		struct {
+			uint32_t  l_i_reserved1;
+		} linux1;
+		struct {
+			uint32_t  h_i_translator;
+		} hurd1;
+		struct {
+			uint32_t  m_i_reserved1;
+		} masix1;
+	} osd1;				/* OS dependent 1 */
+	uint32_t	i_block[EXT2_N_BLOCKS];/* Pointers to blocks */
+	uint32_t	i_generation;	/* File version (for NFS) */
+	uint32_t	i_file_acl;	/* File ACL */
+	uint32_t	i_dir_acl;	/* Directory ACL */
+	uint32_t	i_faddr;	/* Fragment address */
+	union {
+		struct {
+			uint8_t	l_i_frag;	/* Fragment number */
+			uint8_t	l_i_fsize;	/* Fragment size */
+			uint16_t	i_pad1;
+			uint16_t	l_i_uid_high;	/* these 2 fields    */
+			uint16_t	l_i_gid_high;	/* were reserved2[0] */
+			uint32_t	l_i_reserved2;
+		} linux2;
+		struct {
+			uint8_t	h_i_frag;	/* Fragment number */
+			uint8_t	h_i_fsize;	/* Fragment size */
+			uint16_t	h_i_mode_high;
+			uint16_t	h_i_uid_high;
+			uint16_t	h_i_gid_high;
+			uint32_t	h_i_author;
+		} hurd2;
+		struct {
+			uint8_t	m_i_frag;	/* Fragment number */
+			uint8_t	m_i_fsize;	/* Fragment size */
+			uint16_t	m_pad1;
+			uint32_t	m_i_reserved2[2];
+		} masix2;
+	} osd2;				/* OS dependent 2 */
+};
+
+/*
+ * The new version of the directory entry.  Since EXT2 structures are
+ * stored in intel byte order, and the name_len field could never be
+ * bigger than 255 chars, it's safe to reclaim the extra byte for the
+ * file_type field.
+ */
+struct ext2_dir_entry_2 {
+	uint32_t inode;			/* Inode number */
+	uint16_t rec_len;		/* Directory entry length */
+	uint8_t name_len;		/* Name length */
+	uint8_t file_type;
+	char	name[];			/* File name, up to EXT2_NAME_LEN */
 };
 
 int sys_open(const char *filename, int flag, int mode);
--- src/kernel/fs/open.c
+++ src/kernel/fs/open.c
@@ -5,6 +5,10 @@
 
 const uint32_t raam_root_starting_sector = 997109760;
 
+#define INODE_SIZE	256	/* in bytes */
+
+uint32_t find_file(const uint8_t *buf, const char *file_name);
+
 int sys_open(const char *filename, int flag, int mode)
 {
 	char only_file_name[100];
@@ -20,6 +24,31 @@ int sys_open(const char *filename, int flag, int mode)
 	uint32_t bg_inode_table = get_inode_table_start_block();
 
 	printk("inode_table_start_block = {d}  ", bg_inode_table);
+
+
+	/* read inode no.2 i.e. the root inode to get root data */
+
+	uint32_t inode_table_sector = raam_root_starting_sector + (bg_inode_table * 8);
+
+	char *buf = nvme_read(inode_table_sector, 1);
+
+	struct ext2_inode *root_inode = (struct ext2_inode *) (buf + INODE_SIZE);
+
+	uint32_t direct_block_pointer_0 = root_inode->i_block[0];
+
+	printk("direct_block_pointer_0: {d}  ", direct_block_pointer_0); 
+
+	
+	/* read root dir data */
+		
+	uint32_t root_dir_data_sector = raam_root_starting_sector + (direct_block_pointer_0 * 8);
+	buf = nvme_read(root_dir_data_sector, 8);
+
+	uint32_t file_inode = find_file((uint8_t *) buf, only_file_name);	
+
+	if(file_inode != 0) {
+		printk("found the file. Inode = {d}  ", file_inode);
+	}
 
 	return 1;
 }
@@ -39,4 +68,36 @@ static uint32_t get_inode_table_start_block(void)
 	struct ext2_group_desc *desc = (struct ext2_group_desc *) buf;	
 
 	return desc->bg_inode_table;
+}
+
+/* returns file inode or 0 if file not found */
+uint32_t find_file(const uint8_t *buf, const char *file_name)
+{
+	uint8_t *ptr = buf;
+	uint8_t *end = buf + 4096;	/* 1 block of data */	
+	
+	while (ptr < end) {
+		struct ext2_dir_entry_2 *entry = (struct ext2_dir_entry_2 *) ptr;
+
+		/* skip deleted/unused inodes */
+		if(entry->inode == 0) {
+			ptr += entry->rec_len;
+			continue;
+		}
+
+		char name[256];
+
+		strncpy(name, entry->name, entry->name_len);
+
+		name[entry->name_len] = '\0';
+
+		if (strncmp(name, file_name, entry->name_len) == 0) {
+			return entry->inode;
+		}
+
+		ptr += entry->rec_len;
+
+	}
+
+	return 0;
 }
