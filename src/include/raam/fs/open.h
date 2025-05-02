34fe50d4a99abf5036a9b2ab8133becee1a5b60b
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2025-05-02 14:05:46 +0530

    get inode table start block

--- src/include/raam/fs/open.h
+++ src/include/raam/fs/open.h
@@ -1,7 +1,25 @@
 #ifndef OPEN_H
 #define OPEN_H
 
+#define EXT2_BLOCK_SIZE		4096	/* in bytes */
+
+/*
+ * Structure of a blocks group descriptor
+ */
+struct ext2_group_desc
+{
+	uint32_t	bg_block_bitmap;		/* Blocks bitmap block */
+	uint32_t	bg_inode_bitmap;		/* Inodes bitmap block */
+	uint32_t	bg_inode_table;		/* Inodes table block */
+	uint16_t	bg_free_blocks_count;	/* Free blocks count */
+	uint16_t	bg_free_inodes_count;	/* Free inodes count */
+	uint16_t	bg_used_dirs_count;	/* Directories count */
+	uint16_t	bg_pad;
+	uint32_t	bg_reserved[3];
+};
+
 int sys_open(const char *filename, int flag, int mode);
 static void get_only_file_name(const char *filename, const char *only_file_name);
+static uint32_t get_inode_table_start_block(void);
 
 #endif	/* OPEN_H */
--- src/kernel/fs/open.c
+++ src/kernel/fs/open.c
@@ -1,6 +1,9 @@
 #include <lib/string.h>
 #include <raam/printk.h>
 #include <raam/fs/open.h>
+#include <raam/nvme.h>
+
+const uint32_t raam_root_starting_sector = 997109760;
 
 int sys_open(const char *filename, int flag, int mode)
 {
@@ -10,6 +13,13 @@ int sys_open(const char *filename, int flag, int mode)
 
 	printk("only_file_name: ");
 	printk(only_file_name);
+	printk("  ");
+
+	/* get inode table start block address for block group 0 */
+
+	uint32_t bg_inode_table = get_inode_table_start_block();
+
+	printk("inode_table_start_block = {d}  ", bg_inode_table);
 
 	return 1;
 }
@@ -19,3 +29,14 @@ static void get_only_file_name(const char *filename, const char *only_file_name)
 	strncpy(only_file_name, filename + 1, strlen(filename));
 }
 
+static uint32_t get_inode_table_start_block(void)
+{
+	/* block group desc table starts at block 1 */
+	uint32_t group_desc_sector = raam_root_starting_sector + 8;
+	/* read block group desc table for block group 0 */
+	char *buf = nvme_read(group_desc_sector, EXT2_BLOCK_SIZE / 512);
+
+	struct ext2_group_desc *desc = (struct ext2_group_desc *) buf;	
+
+	return desc->bg_inode_table;
+}
