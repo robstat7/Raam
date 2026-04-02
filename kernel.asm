ae487400a7b068e0422bfabe64ccf6555d29e42f
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2026-04-02 12:52:17 +0530

    Refactor code

--- kernel.asm
+++ kernel.asm
@@ -227,23 +227,37 @@ default_tty_init:
 
   ret
 
+;
+; printk
+;
+; this function prints the passed format string onto the terminal
+; screen. It can be called as follows:
+;
+;		printk("Raam Raam sa");
+;
+; args:
+;   @rax = address of the format string defined as a byte string
+;
+; returns:
+;   nothing
+;
 printk:
   mov dword [i], 0
-  push rax
 
 .loop_start:
-  pop rax
   xor ebx, ebx
   mov ebx, dword [i]
   add rbx, rax
 
-  cmp byte [rbx], 0x00
+  cmp byte [rbx], 0x00      ; is string terminated?
   je .exit
 
   push rax
 
   mov al, byte [rbx]
   call tty_put_char
+
+  pop rax
 
   inc dword [i]
   jmp .loop_start
