578bac494242afa308440f936e93ab49db7a83bf
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2026-09-08 07:24:33 +0530

    shell.asm: Add 'ls' command to the list of available commands

--- src/shell.asm
+++ src/shell.asm
@@ -104,7 +104,7 @@ cmd_2 db "clear", 10, 0
 cmd_3 db "echo", 10, 0
 cmd_4 db "reboot", 10, 0
 
-cmd_1_response db "Available commands:", 10, "help", 10, "clear", 10, "echo", \
-10, "reboot", 10, 0
+cmd_1_response db "Available commands:", 10, "help", 10, "ls", 10, "clear", \
+10, "echo", 10, "reboot", 10, 0
 
 cmd_not_found_msg db "Command not found", 10, 0
